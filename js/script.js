be0fb09fa2c5ff5dafa168f96f1a7f4456f9ac64
Maryam Shiddiqah <maryam.shiddiqah@gmail.com>
2023-09-23 11:54:08 +0700

    update changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+// Script Tampilan Menu
 const menuIcon = document.getElementById("menu-icon");
 const menuList = document.getElementById("menu-list");
 
 menuIcon.addEventListener("click", () => {
-    menuList.classList.toggle("hidden");
+    menuList.classList.toggle("hidden"); //menambahkan kelas CSS "hidden" jika belum ada pada elemen tersebut, atau menghapusnya jika sudah ada
 });
 
-
-var swiper = new Swiper(".promo-slider", {
+// Script Slider dengan Swiper
+document.addEventListener("DOMContentLoaded", function () {
+// Inisialisasi slider
+  var mySwiper = new Swiper(".promo-slider", {
+    slidesPerView: 1, // Jumlah slide yang ditampilkan dalam satu tampilan
+    spaceBetween: 30, // Jarak antara slide
+    loop: true, // Aktifkan looping slider
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next", // Selector tombol berikutnya
+      prevEl: ".swiper-button-prev", // Selector tombol sebelumnya
+    },
+    autoplay: {
+      delay: 5000, // Durasi antara slide dalam milidetik
     },
   });
+});
